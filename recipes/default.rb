49518811d0b59daa1f1186b5415823db110cf259
Nick Maiorsky <nickma@gmail.com>
2014-03-18 15:01:10 -0700

    Suse Zyppoer

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,17 +9,14 @@
 log "adding repository"
 
 # check before install that a suitable key is provided, if not bail out
-# Chef::Application.fatal!("Zend Server Order number has to be supplied", 2) if node[:zendserver][:ordernumber].nil? || node[:zendserver][:ordernumber].empty?
-# Chef::Application.fatal!("Zend Server Serial has to be supplied", 2) if node[:zendserver][:licensekey].nil? || node[:zendserver][:licensekey].empty?
+Chef::Application.fatal!("Zend Server Order number has to be supplied", 2) if node[:zendserver][:ordernumber].nil? || node[:zendserver][:ordernumber].empty?
+Chef::Application.fatal!("Zend Server Serial has to be supplied", 2) if node[:zendserver][:licensekey].nil? || node[:zendserver][:licensekey].empty?
 
 version = node[:zendserver][:version]
 phpversion = node[:zendserver][:phpversion]
-url = node[:zendserver][:url]
-basedirdeb = node[:zendserver][:basedirdeb]
-basedirrpm = node[:zendserver][:basedirrpm]
 
 case node[:zendserver][:nginx]
-when true
+when true 
 package_name = "zend-server-nginx-php-#{phpversion}"
 when false
 package_name = "zend-server-php-#{phpversion}"
@@ -27,14 +24,15 @@ else
 package_name = "zend-server-php-#{phpversion}"
 end
 
+puts #{package_name}
+
 case node["platform_family"]
 when "debian"
   include_recipe "apt::default"
   # do things on debian-ish platforms (debian, ubuntu, linuxmint)
-
-	log "Url: #{url}#{version}/#{basedirdeb}/"
+	log "Url: http://repos.zend.com/zend-server/#{version}/deb/"
 	apt_repository "zend-server" do
-	  uri "#{url}#{version}/#{basedirdeb}/"
+	  uri "http://repos.zend.com/zend-server/#{version}/deb/"
 	  components ["server","non-free"]
 	  key "http://repos.zend.com/zend.key"
 	  action :add
@@ -49,33 +47,39 @@ when "rhel"
   # do things on RHEL platforms (redhat, centos, scientific, etc)
   yum_repository "zend-server" do
 	description "Zend Server repo"
-	url "#{url}/#{version}/#{basedirrpm}/$basearch"
+	url "http://repos.zend.com/zend-server/#{version}/rpm/$basearch"
   	action :add
   end
-
+  # Do things on Suse using Zypper
+  
   yum_repository "zend-server-noarch" do
 	description "Zend Server repo"
-	url "#{url}/#{version}/#{basedirrpm}/noarch"
+	url "http://repos.zend.com/zend-server/#{version}/rpm/noarch"
   	action :add
+  end
+when "suse"
+  zypper_repository "zend-server" do
+    uri "http://repos.zend.com/zend-server/#{version}/sles/ZendServer-x86_64"
+    key "http://repos.zend.com/zend.key"
+    title "#{version}"
   end
 end
 
 log "Starting install for package #{package_name}"
 package package_name do
 	:install
-  notifies :restart, 'service[zend-server]', :immediate if node["platform_family"] == "rhel"
 end
 
 #HOT FIX for bug ZSRV-10761 at line 4
 # options.noCache = true;
-#template "/usr/local/zend/gui/public/js/zswebapi.js" do
-#  source "zswebapi.js.erb"
-#  mode 0644
-#  owner "root"
-#  group "root"
-#end
+template "/usr/local/zend/gui/public/js/zswebapi.js" do
+  source "zswebapi.js.erb"
+  mode 0644
+  owner "root"
+  group "root"
+end
 
 # Problem with CentOS api functions if server not restarted
 service "zend-server" do
-	action :nothing
+	action :restart
 end
