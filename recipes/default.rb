3a806263713ab2f563ca509cb0cdf34b6e032dd0
Maurice Kherlakian <mk@vivaxis.ca>
2014-08-26 14:28:00 -0400

    merged back a change taht allows to change the basedir

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,9 @@
 #
 version = node[:zendserver][:version]
 phpversion = node[:zendserver][:phpversion]
+url = node[:zendserver][:url]
+basedirdeb = node[:zendserver][:basedirdeb]
+basedirrpm = node[:zendserver][:basedirrpm]
 
 case node[:zendserver][:nginx]
 when true 
@@ -18,69 +21,46 @@ else
 package_name = "zend-server-php-#{phpversion}"
 end
 
-puts #{package_name}
-
 case node["platform_family"]
 when "debian"
   include_recipe "apt::default"
   # do things on debian-ish platforms (debian, ubuntu, linuxmint)
-	log "Url: http://repos.zend.com/zend-server/#{version}/deb/"
-	apt_repository "zend-server" do
-	  uri "http://repos.zend.com/zend-server/#{version}/deb/"
-	  components ["server","non-free"]
-	  key "http://repos.zend.com/zend.key"
-	  action :add
-	  notifies :run, "execute[apt-get update]", :immediately
+
+  log "Url: #{url}#{version}/#{basedirdeb}/"
+  apt_repository "zend-server" do
+    uri "#{url}#{version}/#{basedirdeb}/"
+    components ["server","non-free"]
+    key "http://repos.zend.com/zend.key"
+    action :add
+    notifies :run, "execute[apt-get update]", :immediately
     end
 when "rhel"
-  
-  # do things on RHEL platforms (redhat, centos, scientific, etc)
-  yum_repository "zend-server" do
-    description "Zend Server repo"
-    gpgkey "http://repos.zend.com/zend.key"
-    url "http://repos.zend.com/zend-server/#{version}/rpm/$basearch"
-    action :add
-  end
-  
-  yum_repository "zend-server-noarch" do
-    description "Zend Server repo"
-    gpgkey "http://repos.zend.com/zend.key"
-    url "http://repos.zend.com/zend-server/#{version}/rpm/noarch"
+  yum_key "zend-server" do
+    url "http://repos.zend.com/zend.key"
     action :add
   end
 
-# Do things on Suse using Zypper
-#when "suse"
-#  include_recipe "zypper::default"
-#  zypper_repository "zend-server" do
-#        uri "http://repos.zend.com/zend-server/#{version}/sles/ZendServer-x86_64"
-#        key "http://repos.zend.com/zend.key"
-#        title "zend-server-#{version}"
-#        action :add
-#  end
-#  zypper_repository "zend-server-noarch" do
-#        uri "http://repos.zend.com/zend-server/#{version}/sles/ZendServer-noarch"
-#        key "http://repos.zend.com/zend.key"
-#        title "zend-server-noarch-#{version}"
-#        action :add
-#  end
+  # do things on RHEL platforms (redhat, centos, scientific, etc)
+  yum_repository "zend-server" do
+  description "Zend Server repo"
+  url "#{url}/#{version}/#{basedirrpm}/$basearch"
+    action :add
+  end
+
+  yum_repository "zend-server-noarch" do
+  description "Zend Server repo"
+  url "#{url}/#{version}/#{basedirrpm}/noarch"
+    action :add
+  end
 end
 
 log "Starting install for package #{package_name}"
 package package_name do
-	:install
-end
-
-#HOT FIX for bug ZSRV-10761 at line 4
-# options.noCache = true;
-template "/usr/local/zend/gui/public/js/zswebapi.js" do
-  source "zswebapi.js.erb"
-  mode 0644
-  owner "root"
-  group "root"
+  :install
+  notifies :restart, 'service[zend-server]', :immediate if node["platform_family"] == "rhel"
 end
 
 # Problem with CentOS api functions if server not restarted
 service "zend-server" do
-	action :restart
+  action :nothing
 end
