5b12709532e335ea0e6ebc3704677fcdcfb3f8d7
Maurice Kherlakian <mk@vivaxis.ca>
2015-05-06 09:49:16 -0400

    upped version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'maurice.k@zend.com'
 license          'All rights reserved'
 description      'Installs/Configures zendserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.5'
+version          '1.1.6'
 
 depends "apt"
 depends "yum"
